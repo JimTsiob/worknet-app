58d5fe2158f262d6346229e067d39111698e1c8c
Konstantinos Syrios <kostassir@yahoo.gr>
2024-05-20 23:09:52 +0300

    Implemented skeleton for Login and Register activities

--- worknet-android/app/src/main/java/com/syrtsiob/worknet/Login.java
+++ worknet-android/app/src/main/java/com/syrtsiob/worknet/Login.java
@@ -1,6 +1,11 @@
 package com.syrtsiob.worknet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    Button buttonLogin;
+    EditText inputEmail;
+    EditText inputPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        inputEmail = findViewById(R.id.editTextTextEmailAddress);
+        inputPassword = findViewById(R.id.editTextTextPassword);
+
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(listener -> {
+            AttemptLogin();
         });
+    }
+
+    private void AttemptLogin() {
+      String email = inputEmail.getText().toString();
+      String password = inputPassword.getText().toString();
+
+      if(AuthenticateUser(email, password)) {
+        finishAffinity();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getResources().getString(R.string.e_mail), email); // TODO add any other extras
+        startActivity(intent);
+      }
+      else {
+          Toast.makeText(this, R.string.authentication_fail_msg, Toast.LENGTH_LONG).show();
+      }
+    }
+
+    private boolean AuthenticateUser(String email, String password) {
+        // TODO implement user authentication
+        return true;
     }
 }
--- worknet-android/app/src/main/java/com/syrtsiob/worknet/MainActivity.java
+++ worknet-android/app/src/main/java/com/syrtsiob/worknet/MainActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.syrtsiob.worknet.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
     }
 }
