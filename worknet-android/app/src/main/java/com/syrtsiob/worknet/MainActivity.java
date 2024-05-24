fe8408cbebf90a98485ad1e7a0695025427ae63c
Konstantinos Syrios <kostassir@yahoo.gr>
2024-05-25 02:01:51 +0300

    Added top bar and navigation view

--- worknet-android/app/src/main/java/com/syrtsiob/worknet/MainActivity.java
+++ worknet-android/app/src/main/java/com/syrtsiob/worknet/MainActivity.java
@@ -1,11 +1,18 @@
 package com.syrtsiob.worknet;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,7 +23,7 @@ import com.syrtsiob.worknet.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    ActionBar actionBar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
-
-        actionBar = getSupportActionBar();
-        if(actionBar!=null) {
-            actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.ic_launcher_background));
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemID = item.getItemId();
@@ -46,6 +47,36 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        findViewById(R.id.profileImage).setOnClickListener(listener -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+        binding.navigationView.setNavigationItemSelectedListener(item->{
+            int itemID = item.getItemId();
+            if(itemID == R.id.profile)
+                replaceFragment(new ProfileFragment());
+            else if(itemID == R.id.settings)
+                replaceFragment(new SettingsFragment());
+            drawerLayout.close();
+            return true;
+        });
+
+        findViewById(R.id.messages).setOnClickListener(listener -> {
+            replaceFragment(new MessagesFragment());
+            clearBottomMenuSelection();
+        });
+
+        EditText searchBar = findViewById(R.id.searchBar);
+        searchBar.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                replaceFragment(new SearchResultsFragment());
+                searchBar.clearFocus();
+            }
+            return false;
+        });
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -53,5 +84,12 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.mainFrame, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void clearBottomMenuSelection(){
+        Menu menu = binding.bottomNavigationView.getMenu();
+        for (int i = 0; i < menu.size(); i++) {
+            menu.getItem(i).setChecked(false);
+        }
     }
 }
