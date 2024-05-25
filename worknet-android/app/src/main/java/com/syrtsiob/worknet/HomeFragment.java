4aa33f51d1178c2efd0433175f9325331a51d9a5
Konstantinos Syrios <kostassir@yahoo.gr>
2024-05-25 21:58:01 +0300

    Implemented a prototype for the home page with posts and their popups

--- worknet-android/app/src/main/java/com/syrtsiob/worknet/HomeFragment.java
+++ worknet-android/app/src/main/java/com/syrtsiob/worknet/HomeFragment.java
@@ -6,10 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -29,6 +33,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private LinearLayout postsContainer;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -75,7 +81,59 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView textView = getView().findViewById(R.id.label);
-        textView.setText(mParam1 + " - " + mParam2);
+        postsContainer = getView().findViewById(R.id.postsContainer);
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+        addPost();
+    }
+
+    // TODO Add post content and functionality
+    private void addPost() {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View postView = inflater.inflate(R.layout.post_template, postsContainer, false);
+
+        // TODO add images
+
+        TextView postText = postView.findViewById(R.id.post_text);
+        Button likeButton = postView.findViewById(R.id.post_like_button);
+        Button commentsButton = postView.findViewById(R.id.post_comments_button);
+
+        commentsButton.setOnClickListener(listener -> {
+            postPopup(postsContainer);
+        });
+
+        postsContainer.addView(postView);
+    }
+
+    private void postPopup(View view) {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View popupView = inflater.inflate(R.layout.post_popup_template, postsContainer, false);
+
+        LinearLayout postPopupCommentContainer = popupView.findViewById(R.id.postPopupCommentContainer);
+        for (int i = 0; i < 15; i++) {
+            View comment = inflater.inflate(R.layout.post_popup_comment_template, postPopupCommentContainer, false);
+            TextView commentText = comment.findViewById(R.id.comment_text);
+            commentText.setText("Comment " + i);
+            postPopupCommentContainer.addView(comment);
+        }
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
     }
 }
